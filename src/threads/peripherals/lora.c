efe3968c32dce7d328ffb57b5317d5dc7c937dac
lizard123137 <lizard123137@gmail.com>
2025-04-04 12:37:22 +0200

    fixed issue with lora not sending

--- src/threads/peripherals/lora.c
+++ src/threads/peripherals/lora.c
@@ -6,9 +6,16 @@ void *lora(void *arg) {
     int lora_port = serial_get_device(LORA_DEVICE, B9600);
     assert(lora_port > 0);
 
+    char buffer[TSDATA_BUFFER_SIZE + 2];
+
     while(true) {
         const void *data = read_data();
-        write(lora_port, data, 1024);
+
+        memcpy(buffer, data, TSDATA_BUFFER_SIZE);
+        buffer[TSDATA_BUFFER_SIZE] = '\n';
+        buffer[TSDATA_BUFFER_SIZE + 1] = '\r';
+        
+        write(lora_port, buffer, TSDATA_BUFFER_SIZE + 2);
 
         sleep(1);
     }
