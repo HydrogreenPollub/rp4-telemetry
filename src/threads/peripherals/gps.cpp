44d6a856cfce18433e5086f0072a7c6b057624ff
lizard123137 <lizard123137@gmail.com>
2025-05-21 00:04:25 +0200

    added more handled sentences to gps thread

--- src/threads/peripherals/gps.cpp
+++ src/threads/peripherals/gps.cpp
@@ -11,7 +11,14 @@ void* gps(void* arg)
     asio::io_context io;
     asio::serial_port serial(io);
 
-    serial.open(GPS_DEVICE);
+    boost::system::error_code ec;
+    serial.open(GPS_DEVICE, ec);
+
+    if (ec) {
+        std::cout << "GPS: Failed to open device - " << ec.message() << std::endl;
+        return nullptr;
+    }
+
     serial.set_option(asio::serial_port::baud_rate(115200));
     serial.set_option(asio::serial_port::character_size(8));
     serial.set_option(asio::serial_port::parity(asio::serial_port::parity::none));
@@ -26,24 +33,55 @@ void* gps(void* arg)
         std::string line;
         std::getline(input, line);
 
-        // TODO do we need to parse other types of sentences???
+        // TODO add altitude and time to tsdata
         enum minmea_sentence_id sentence_id = minmea_sentence_id(line.c_str(), false);
         switch (sentence_id) {
-        case MINMEA_SENTENCE_RMC: {
+        // Global Positioning System Fix Data
+        case MINMEA_SENTENCE_GGA:
+            struct minmea_sentence_gga frame;
+            if (minmea_parse_gga(&frame, line.c_str())) {
+                std::cout << "GPS: GGA frame received at time - " << frame.time << std::endl;
+
+                set_gpsLatitude(minmea_tocoord(&frame.latitude));
+                set_gpsLongitude(minmea_tocoord(&frame.longitude));
+                // TODO add time
+            }
+            break;
+        // Geographic position, latitude and longitude
+        case MINMEA_SENTENCE_GLL:
+            struct minmea_sentence_gll frame;
+            if (minmea_parse_gll(&frame, line.c_str())) {
+                set_gpsLatitude(minmea_tocoord(&frame.latitude));
+                set_gpsLongitude(minmea_tocoord(&frame.longitude));
+                // TODO add time
+            }
+            break;
+        // GPS Satellites in view
+        case MINMEA_SENTENCE_GSV:
+            struct minmea_sentence_gsv frame;
+            if (minmea_parse_gsv(&frame, line.c_str())) {
+                std::cout << "GPS: Total number of satellites visible - " << frame.total_sats << std::endl;
+            }
+            break;
+        // Recommended minimum specific GPS
+        case MINMEA_SENTENCE_RMC:
             struct minmea_sentence_rmc frame;
             if (minmea_parse_rmc(&frame, line.c_str())) {
                 set_gpsLatitude(minmea_tocoord(&frame.latitude));
                 set_gpsLongitude(minmea_tocoord(&frame.longitude));
-                set_gpsSpeed(minmea_tofloat(&frame.speed));
-                // TODO add altitude and time
+                set_gpsSpeed(minmea_tofloat(&frame.speed) * 1.852); // Convert to km/h from knots
             }
             break;
-        }
+        // Track made good and ground speed
+        case MINMEA_SENTENCE_VTG:
+            struct minmea_sentence_vtg frame;
+            if (minmea_parse_vtg(&frame, line.c_str())) {
+                set_gpsSpeed(minmea_tofloat(&frame.speed_kph));
+            }
+            break;
         default:
             std::cout << "GPS: Unhandled NMEA sentence - ID " << sentence_id << std::endl;
             break;
         }
-
-        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
--- src/threads/peripherals/gps.hpp
+++ src/threads/peripherals/gps.hpp
@@ -4,6 +4,7 @@
 #include <utils/minmea/minmea.h>
 
 #include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
 
 #include <thread>
 #include <iostream>
