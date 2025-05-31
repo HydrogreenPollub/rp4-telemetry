c8b7c1f427f0dc805d93bbd9988c9917eac23ced
lizard123137 <lizard123137@gmail.com>
2025-05-31 15:42:05 +0200

    fixed memory issue

--- src/utils/data.cpp
+++ src/utils/data.cpp
@@ -3,15 +3,20 @@
 capnp::MallocMessageBuilder global_message;
 std::mutex data_mutex;
 
-auto telemetry_data = global_message.initRoot<TSData>();
-
 std::vector<uint8_t> read_data(void)
 {
     std::lock_guard<std::mutex> lock(data_mutex);
-    
-    kj::Array<capnp::word> words = capnp::messageToFlatArray(global_message);
-    kj::ArrayPtr<capnp::word> wordPtr = words.asPtr();
 
-    auto bytes = kj::arrayPtr(reinterpret_cast<const uint8_t*>(wordPtr.begin()), wordPtr.size() * sizeof(capnp::word));
-    return std::vector<uint8_t>(bytes.begin(), bytes.end());
+    kj::Array<capnp::word> words = capnp::messageToFlatArray(global_message);
+    
+    // Get a byte pointer to the start and end of the word array
+    const uint8_t* begin = reinterpret_cast<const uint8_t*>(words.begin());
+    const uint8_t* end = begin + (words.size() * sizeof(capnp::word));
+
+    // Copy into a vector using std::copy
+    std::vector<uint8_t> buffer;
+    buffer.reserve(end - begin);
+    std::copy(begin, end, std::back_inserter(buffer));
+
+    return buffer;
 }
